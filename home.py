720d156a07ff996e5d0e45555f873b5ccac55f6e
Jackson Yeager <jacksonyeager@gmail.com>
2016-09-29 13:36:14 -0400

    Added CRUD actions for admin cp

--- home.py
+++ home.py
@@ -16,13 +16,12 @@ cursor = connection.cursor()
 
 @app.route('/')
 def index():
-
     cursor.execute("SELECT content FROM page_content WHERE page = 'home' AND location = 'header' AND status = 1")
 
     header = cursor.fetchone()
 
-    cursor.execute("SELECT header_text, content, image FROM page_content "
-                   "WHERE page = 'home' and location = 'left-bar' AND status = 1")
+    cursor.execute("SELECT header, content, image FROM page_content "
+                   "WHERE page = 'home' AND location = 'left-bar' AND status = 1")
 
     data = cursor.fetchall()
     return render_template('index.html', header=header, data=data)
@@ -35,7 +34,9 @@ def admin():
 
 @app.route('/login', methods=['POST'])
 def login():
-    if request.form['username'] == 'admin' and request.form['password'] == 'password':
+    if 'username' in session:
+        return redirect('/admin')
+    elif request.form['username'] == 'admin' and request.form['password'] == 'password':
         session['username'] = request.form['username']
         return redirect('/admin')
     else:
@@ -45,7 +46,11 @@ def login():
 @app.route('/admin', methods=['GET'])
 def admin_cp():
     if 'username' in session:
-        return render_template('admin.html', success=request.args.get('success'))
+        home_query = "SELECT id, header, content, image, status FROM page_content WHERE page = 'home'"
+        cursor.execute(home_query)
+        home_data = cursor.fetchall()
+
+        return render_template('admin.html', success=request.args.get('success'), home_data=home_data)
     else:
         return redirect('/login?failure=true')
 
@@ -57,18 +62,56 @@ def submit():
         header = request.form['header']
         image = request.form['image']
 
-        query = "INSERT INTO page_content VALUES (DEFAULT, 'home', '%s', 1, 1, 'left_bar', '', '%s', '%s')" \
-                % (body, header, image)
+        query = "INSERT INTO page_content VALUES (DEFAULT, 'home', %s, 1, 1, 'left_bar', '', %s, %s)"
 
-
-
-        cursor.execute(query)
+        cursor.execute(query, (body, header, image))
         connection.commit()
 
         return redirect('/admin?success=true')
 
     else:
         return redirect('/login?failure=true')
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
+
+
+@app.route('/edit/<id>', methods=['GET'])
+def edit(id):
+    query = "SELECT id, header, content, image, status, priority FROM page_content WHERE id = %s"
+    cursor.execute(query, id)
+    print query
+    data = cursor.fetchone()
+
+    return render_template('edit.html', data=data)
+
+
+@app.route('/edit/<id>', methods=['POST'])
+def update(id):
+    query = "UPDATE page_content SET header = %s, content = %s, image = %s WHERE id = %s"
+    cursor.execute(query, (request.form['header'], request.form['body'], request.form['image'], id))
+    connection.commit()
+    return redirect('/admin')
+
+
+@app.route('/delete/<id>', methods=['GET'])
+def delete(id):
+    query = "DELETE FROM page_content WHERE id = %s"
+    cursor.execute(query, id)
+    connection.commit()
+    return redirect('/admin')
+
+
+@app.route('/test', methods=['POST'])
+def test():
+    query = "SELECT * FROM page_content WHERE image = " + request.form['rekt']
+    print query
+    cursor.execute(query)
+    connection.commit()
+    return redirect('/admin')
 
 
 if __name__ == '__main__':
